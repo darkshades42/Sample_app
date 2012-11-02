8d04337812c90e190ed43e80c0cadb0209c45e54
Jordan Steeves <jeeperscreepers28@gmail.com>
2012-11-02 12:37:09 -0700

    even more to production.rb file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,13 +15,10 @@ SampleApp::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
-
-  # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( public.js public.css blueprint/screen.css blueprint/print.css )
 
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
